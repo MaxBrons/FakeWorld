0c56896f983ff6ab7a1a31e5afc5d3f5a9738d94
Max Bronstring <max.brons@hotmail.com>
2019-11-11 11:58:28 +0100

    Start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 3;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.position += transform.right * speed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            transform.position += -transform.up * speed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.D))
+            transform.position += -transform.right * speed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.W))
+            transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
